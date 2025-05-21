3e802fafe94979c203d5c00452f7b81d4d522f28
aquaman122 <swj981128@gmail.com>
2025-05-21 16:26:33 +0900

    feat: 로켓 과제테스트

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -7,11 +7,11 @@ export default function Home() {
         <img src="/fullImage.png" alt="webtoonImage" />
         <div className="absolute top-[33.2%] left-[34.2%] transform -translate-x-1/2 -translate-y-1/2 text-center text-md small:text-xl">
           이제 본격적으로<br />
-          석원님의 사주팔자를<br />
+          로켓님의 사주팔자를<br />
           분석해볼 차례네요.
         </div>
         <div className="absolute top-[50.4%] left-[37.5%] transform -translate-x-1/2 -translate-y-1/2 text-center text-md small:text-xl">
-          제가 석원님의 사주를<br />
+          제가 로켓님의 사주를<br />
           보기 쉽게 표로 정리했어요.
         </div>
       </div>
